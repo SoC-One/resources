83c89526778291a1c57dd20a0d8da30116b82efb
lapd <lapd@soc.one>
2024-06-13 16:37:53 +0700

    added support both centos and ubuntu
    Signed-off-by: lapd <lapd@soc.one>

--- scripts/kasm.sh
+++ scripts/kasm.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 KASM_VERSION=1.3.1
-UBUNTU_VERSION=$(lsb_release -c | awk '{print $2}')
-mkdir -p /tmp/kasm-cache
-cd /tmp/kasm-cache
-wget https://github.com/kasmtech/KasmVNC/releases/download/v${KASM_VERSION}/kasmvncserver_${UBUNTU_VERSION}_${KASM_VERSION}_amd64.deb
-sudo apt-get install ./kasmvncserver_*.deb
-sudo apt install -y ubuntu-mate-desktop
-sudo addgroup $USER ssl-cert
-vncserver -select-de mate
 
+# Detecting the Linux distribution
+if [ -x "$(command -v lsb_release)" ]; then
+    DISTRO=$(lsb_release -i -s)
+    case "$DISTRO" in
+        Ubuntu)
+            UBUNTU_VERSION=$(lsb_release -c -s)
+            mkdir -p /tmp/kasm-cache
+            cd /tmp/kasm-cache
+            wget https://github.com/kasmtech/KasmVNC/releases/download/v${KASM_VERSION}/kasmvncserver_${UBUNTU_VERSION}_${KASM_VERSION}_amd64.deb
+            sudo apt-get install ./kasmvncserver_*.deb
+            sudo apt install -y ubuntu-mate-desktop
+            sudo addgroup $USER ssl-cert
+            ;;
+        CentOS)
+            mkdir -p /tmp/kasm-cache
+            cd /tmp/kasm-cache
+            wget https://github.com/kasmtech/KasmVNC/releases/download/v${KASM_VERSION}/kasmvncserver_centos_core_${KASM_VERSION}_x86_64.rpm
+            sudo yum localinstall -y kasmvncserver_centos_core_${KASM_VERSION}_x86_64.rpm
+            sudo yum groupinstall -y "MATE Desktop"
+            sudo usermod -aG ssl-cert $USER
+            newgrp ssl-cert
+            ;;
+        *)
+            echo "Unsupported distribution: $DISTRO"
+            exit 1
+            ;;
+    esac
+else
+    echo "lsb_release command not found. Cannot determine distribution."
+    exit 1
+fi
+
+# Configuring VNC server
+vncserver -kill :1   # Kill any existing VNC server on display :1 (if exists)
+
+# Writing VNC server configuration to file
 cat > ~/.vnc/kasmvnc.yaml << EOL
 network:
   protocol: http
@@ -18,6 +46,8 @@ network:
     public_ip: 127.0.0.1
 EOL
 
+
+# Setting up systemd service for VNC server
 cat > vncserver@:1.service << EOL
 [Unit]
 Description=Remote desktop service (VNC)
