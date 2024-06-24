6d0dbc8eeb4825f6674b71ab8a17ae79471d06c3
lapd <lapd@soc.one>
2024-06-24 11:55:32 +0700

    Updated
    Signed-off-by: lapd <lapd@soc.one>

--- scripts/kasm.sh
+++ scripts/kasm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 KASM_VERSION=1.3.1
-
+USER=administrator
+KASM_VNC_PASSWD=administrator
 # Detecting the Linux distribution
 if [ -x "$(command -v lsb_release)" ]; then
     DISTRO=$(lsb_release -i -s)
@@ -14,16 +15,40 @@ if [ -x "$(command -v lsb_release)" ]; then
             sudo apt-get install ./kasmvncserver_*.deb
             sudo apt install -y ubuntu-mate-desktop
             sudo addgroup $USER ssl-cert
+
+            echo -e "$KASM_VNC_PASSWD\n$KASM_VNC_PASSWD\n" | kasmvncpasswd -u $USER -w "/home/$USER/.kasmpasswd"
+            sudo chown -R 1000:0 "/home/$USER/.kasmpasswd"
+            sudo addgroup $USER ssl-cert
+            sudo -u $USER vncserver -select-de mate
+
             ;;
         CentOS)
             mkdir -p /tmp/kasm-cache
             cd /tmp/kasm-cache
-            sudo yum install -y perl-DateTime perl-DateTime-TimeZone wget
+
+            # Centos7
+            sudo yum makecache
+            sudo yum install -y epel-release
+            sudo yum install -y mesa-libGL libXfont2 libXtst pixman perl-Hash-Merge-Simple
+            sudo yum install -y libGL.so.1 libX11.so.6 libXau.so.6 libXcursor.so.1 libXext.so.6 \
+            libXfixes.so.3 libXfont2.so.2 libXrandr.so.2 libXtst.so.6 libpixman-1.so.0 \
+            perl-Data-Dumper perl-Hash-Merge-Simple perl-Switch perl-YAML-Tiny \
+            xkeyboard-config xorg-x11-server-utils xorg-x11-xauth xorg-x11-xkb-utils \
+            perl-DateTime perl-DateTime-TimeZone wget
+            
             wget https://github.com/kasmtech/KasmVNC/releases/download/v${KASM_VERSION}/kasmvncserver_centos_core_${KASM_VERSION}_x86_64.rpm
-            sudo yum localinstall -y kasmvncserver_centos_core_${KASM_VERSION}_x86_64.rpm
+            
+            sudo rpm -ivh kasmvncserver_centos_core_${KASM_VERSION}_x86_64.rpm
             sudo yum groupinstall -y "MATE Desktop"
-            sudo usermod -aG ssl-cert $USER
-            newgrp ssl-cert
+            vncpasswd -u $USER -w -r
+
+            sudo usermod -aG kasmvnc-cert $USER || true
+            newgrp kasmvnc-cert || true
+
+            echo -e "$KASM_VNC_PASSWD\n$KASM_VNC_PASSWD\n" | kasmvncpasswd -u $USER -w "/home/$USER/.kasmpasswd"
+            sudo chown -R 1000:0 "/home/$USER/.kasmpasswd"
+            sudo -u $USER vncserver -select-de mate
+
             ;;
         *)
             echo "Unsupported distribution: $DISTRO"
@@ -36,9 +61,10 @@ else
 fi
 
 # Configuring VNC server
-vncserver -kill :1   # Kill any existing VNC server on display :1 (if exists)
+vncserver -kill :1 || true  # Kill any existing VNC server on display :1 (if exists)
 
 # Writing VNC server configuration to file
+mkdir -p ~/.vnc
 cat > ~/.vnc/kasmvnc.yaml << EOL
 network:
   protocol: http
@@ -70,3 +96,5 @@ sudo systemctl daemon-reload
 sudo systemctl restart vncserver@:1.service
 sudo systemctl  enable vncserver@:1.service
 sudo systemctl status vncserver@:1.service
+
+sudo rm -rf /tmp/kasm-cache
